f80f0b6b42fe500ff2a095d79ab87affd9f5a874
Dev <devvarshney11@gmail.com>
2023-10-04 23:17:01 +0530

    Lesson 1 commit 2

---
+++ ReactLesson1/App.js
@@ -0,0 +1,11 @@
+const heading1 = React.createElement('h1', {
+    id : "tittle1"
+}, 'Ram Ram Ji Saryane');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading2 = React.createElement('h2', {
+    id : "tittle2"
+},"To aaj hai humara react seekhne ka phela din")
+const container = React.createElement('div',{
+    id:"container"
+},[heading1,heading2])
+root.render(container);
